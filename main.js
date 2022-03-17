5a8ae25d5fc313076f11a5a9c6d3ef0298fd4ccc
S M Iftekhar Uddin <Uddin.SMI@gmail.com>
2022-03-17 18:22:25 -0400

    as of mar 17 622pm

--- main.js
+++ main.js
@@ -1,41 +1,88 @@
 
 
 //Features remaining:
-//Readme 
-//Sounds!
-//Background change
-//Client can modify: (1) defaultTime (2) Sound on/off (3) Which sites to track
+//Google audio issue + check if others can hear audio
+//Client can modify: (1) defaultTime (2) Sound on/off (3) Which sites to track (4) Sound options: off or choose from list
 
 
 //Stretch features:
+    //Readme - need to publish to Google Chrome Extensions page
     //save number of times client got alerts for a website -> retrieve total no. of mins spent on said website
     
-
 //if client goes to any site in the matches array 
     //open main.js
     //time tracker automatically set off usign setTimeout
     //when time spent > defaultTime -> set alert
     
-    const defaultTime = 7000;
 
-    setTimeout(function() {
-        alert("You\'ve exceeded your screen time. Get to code!");
-        window.location.replace("https://leetcode.com/problemset/all/");
-    }, defaultTime);
+    let myAudio = new Audio();   
+    myAudio.src = chrome.extension.getURL("sound1.mp3");   
+    
 
-    //client can modify default time dynamically
+    // const defaultTime = 7000;
 
-    //change background after alert if possible:
     // setTimeout(function() {
-    //     if (counter === 1) {window.location.replace("http://www.w3schools.com")}    
-    // }, 2000);
+        
+    //     myAudio.play();
+    //     alert("You\'ve exceeded your screen time. Get to code!");
+    //     window.location.replace("https://leetcode.com/problemset/all/");
+    // }, defaultTime);
+
+    //Time out options page for JS
+    // Saves options to chrome.storage
+    function save_options() {
+    let timeVal = document.getElementById('time').value;
+    let likesColor = document.getElementById('like').checked;
+    //storing to chrome.storage
+    chrome.storage.sync.set({
+      favoriteTime: timeVal,
+      likesColor: likesColor
+    }, function() {
+      // Update status to let user know options were saved.
+      let status = document.getElementById('status');
+      status.textContent = 'Options saved.';
+      setTimeout(function() {
+        status.textContent = '';
+      }, 750);
+    });
+  }
+  
+  // Restores select box and checkbox state using the preferences
+  // stored in chrome.storage.
+  function restore_options() {
+    // Use default value timeVal = '5' and likesColor = true.
+    chrome.storage.sync.get({
+      favoriteTime: 5,
+      likesColor: true
+    }, function(items) {
+      document.getElementById('time').value = items.favoriteTime;
+      document.getElementById('like').checked = items.likesColor;
+    });
+  }
+
+
+  document.addEventListener('DOMContentLoaded', restore_options);
+  document.getElementById('save').addEventListener('click',
+      save_options);
     
 
 
+//defaultTime is what we use in setTimeout
+//1min = 60,000ms
+//30s aka 0/5 min = 30,000ms
+//defaultTime = timeVal/60000
 
-//Read me page or chrome-extension page for Screen Time(TM)
-    //allow client to choose or add which sites to track (in addition to deault sites)
-    //allow client to choose time monitored (default 15 mins)
+const defaultTime = 7000;
+// const defaultTime = items.favoriteTime*60000;
+//items.favoriteTime = 0.5?
+
+setTimeout(function() {
+        
+    myAudio.play();
+    alert("You\'ve exceeded your screen time. Get to code!");
+    window.location.replace("https://leetcode.com/problemset/all/");
+}, defaultTime);
+
 
 
 
